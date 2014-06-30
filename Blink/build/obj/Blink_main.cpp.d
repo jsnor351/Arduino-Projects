482af76c06404dba4fbdc6f7d1396cba8bda0ab8
jsnor351 <jsnor351@gmail.com>
2014-06-30 11:51:21 -0500

    Blink
    Universal arduino starter code done in MariaMole v 0.5.5.0

---
+++ Blink/build/obj/Blink_main.cpp.d
@@ -0,0 +1,13 @@
+e:\githubarduino\arduino-projects\Blink\build\obj\Blink_main.cpp.o:  \
+ e:\githubarduino\arduino-projects\Blink\source\Blink_main.cpp \
+  e:\githubarduino\arduino-projects\Blink\source\/Blink_main.h \
+  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino/arduino.h \
+  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino/binary.h \
+  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino/WCharacter.h \
+  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino/WString.h \
+  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino/HardwareSerial.h \
+  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino/Stream.h \
+  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino/Print.h \
+  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino/Printable.h \
+  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\cores\arduino/new.h \
+  C:\Program\ Files\ (x86)\Arduino\hardware\arduino\variants\standard/pins_arduino.h
